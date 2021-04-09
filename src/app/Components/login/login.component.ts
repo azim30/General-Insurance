766b6e5dd17fcefd1e5071ea391857aea9bc17ae
miteshsawant07 <37763621+miteshsawant07@users.noreply.github.com>
2021-04-09 17:52:22 +0530

    Update login.component.ts

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import {FormsModule,NgForm,FormGroup} from '@angular/forms';
+import {UserInfoModule} from 'src/app/Modules/user-info/user-info.module';
+import {UserInfoService} from 'src/app/Services/user-info.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +12,40 @@ import {FormsModule,NgForm,FormGroup} from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   model:any=[];
+  svc:UserInfoService;
+  mob:number;
+  pwd:string;
+  ngzone: NgZone;
+  router: Router;
+  constructor(svc: UserInfoService, ngzone: NgZone,
+    router: Router) 
+    {
+      this.svc=svc;
+      this.ngzone = ngzone;
+      this.router = router;
 
-
-  constructor() { }
+    }
 
   ngOnInit(): void {
   }
-  LoginData(logform:NgForm):void{
-    console.log(logform.value);
+  Login(LoginForm:NgForm):void{
+    this.mob = this.model.mno;
+    this.pwd= this.model.txtpass;
+    alert(this.mob+','+this.pwd);
+
+    this.svc.Login(this.mob,this.pwd).subscribe((data:string)=> {
+    console.log(data);
+    if(data=="Login Successful")
+    {
+      alert('Login successful');
+      this.ngzone.run(()=>this.router.navigateByUrl('/BuyInsurance'));
+    }
+    else
+    {
+      alert('Invalid credentials');
+    }
+    });
+
   }
 
 }
