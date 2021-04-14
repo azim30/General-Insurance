a4e13cd6375bc1489a673cdcabc6eaf03c0c20b4
justanothercodingirl <nprdshmkh@gmail.com>
2021-04-14 11:30:26 +0530

    Material Module Added
    Material Module Added

---
+++ src/app/Modules/material/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+
+const MaterialComponents=[
+
+]
+
+@NgModule({
+
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
+
+  
+})
+export class MaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import{HttpClientModule}from'@angular/common/http';
-import { BuyInsuranceComponent } from './Components/buy-insurance/buy-insurance.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule}from'@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MaterialModule} from './Modules/material/material.module';
+/*Components*/
+import { BuyInsuranceComponent } from './Components/buy-insurance/buy-insurance.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { ContactUsComponent } from './Components/contact-us/contact-us.component';
 import { FaqComponent } from './Components/faq/faq.component';
-//<<<<<<< HEAD
 import { ClaiminsuranceComponent } from './Components/claiminsurance/claiminsurance.component';
 
-//=======
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//>>>>>>> fa3d85d5d5f74b70ccbce070f1ed791511d301a0
+
 
 
 @NgModule({
@@ -41,7 +41,8 @@ import { ClaiminsuranceComponent } from './Components/claiminsurance/claiminsura
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    //BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
 
   ],
   providers: [],
