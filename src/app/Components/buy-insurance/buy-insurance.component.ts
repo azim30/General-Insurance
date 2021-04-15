c3280e28901a53d7673937c54f9b8b9fe09c8031
miteshsawant07 <miteshsawant07.ms@gmail.com>
2021-04-15 15:43:13 +0530

    buy insurance updated

--- src/app/Components/buy-insurance/buy-insurance.component.ts
+++ src/app/Components/buy-insurance/buy-insurance.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm} from '@angular/forms';
+import {FormsModule,NgForm,FormGroup} from '@angular/forms';
+import {UserInfoModule} from 'src/app/Modules/user-info/user-info.module';
+import {UserInfoService} from 'src/app/Services/user-info.service';
+import { VehicleinfomoduleModule } from 'src/app/Modules/vehicleinfomodule/vehicleinfomodule.module';
+import { VehicleinfoserviceService } from 'src/app/Services/vehicleinfoservice.service';
+//import { threadId } from 'node:worker_threads';
 @Component({
   selector: 'app-buy-insurance',
   templateUrl: './buy-insurance.component.html',
@@ -8,11 +13,48 @@ import { NgForm} from '@angular/forms';
 })
 export class BuyInsuranceComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  model: any = [];
+  svc:VehicleinfoserviceService;
+  usr = new VehicleinfomoduleModule();
+  ngzone: NgZone;
+  router: Router;
+  
+  constructor(svc: VehicleinfoserviceService, ngzone: NgZone,
+    router: Router) 
+    {
+      this.svc = svc;
+      this.ngzone = ngzone;
+      this.router = router;
+
+    }
 
   ngOnInit(): void {
   }
 
-  
+  vehicleData(vehicleRegisteraionForm: NgForm):void
+  {
+    console.log(vehicleRegisteraionForm.value);
+    //this.usr.VehicleID = buyinsuranceform.value.vid;
+    this.usr.Maufacturer = vehicleRegisteraionForm.value.cbmanu; 
+    this.usr.Model = vehicleRegisteraionForm.value.cbmodel;
+    this.usr.DrivingLicense = vehicleRegisteraionForm.value.lic; 
+    this.usr.VehicleType = vehicleRegisteraionForm.value.vtype;
+    this.usr.RegistrationNo = vehicleRegisteraionForm.value.rno; 
+    this.usr.EngineNo = vehicleRegisteraionForm.value.enum;
+    this.usr.ChassisNo = vehicleRegisteraionForm.value.cnum; 
+    this.usr.PurchaseDate = vehicleRegisteraionForm.value.pdate;
+    this.usr.UserMobNo = vehicleRegisteraionForm.value.umob; 
+    //alert(this.usr.VehicleID+','+this.usr.Maufacturer);
+    this.svc.RegisterVehicle(this.usr).subscribe((data: boolean) =>
+    {
+      //alert(data);
+      if(data==true)
+      {
+        alert('Susscessfully registered your vehicle');
+        this.ngzone.run(()=>this.router.navigateByUrl('/policy'));
 
+      }
+  });
+ 
+ }
 }
