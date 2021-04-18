2566ab15d71296efef206d07089a2841fb84443c
miteshsawant07 <miteshsawant07.ms@gmail.com>
2021-04-18 15:11:11 +0530

    updated

---
+++ src/app/Modules/travelclaiminfo/travelclaiminfo.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class TravelclaiminfoModule {
+  Travel_Claim_Id: number;
+  PolicyNo: number;
+  Reason_for_Claim: string;
+  MobNo: string;
+  Amount: number;
+  Claim_Status: string;
+ }
